e0745edd9078ed89aefd3ce49adbe18c45e2e293
aaron1986 <aaronbrucesmith86@gmail.com>
2023-08-03 13:50:49 +0100

    Add files via upload

---
+++ Areas/Identity/Data/DBContextSample.cs
@@ -0,0 +1,38 @@
+﻿using ManagementSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ManagementSystem.Areas.Identity.Data;
+
+public class DBContextSample : IdentityDbContext<SampleUser>
+{
+    public DBContextSample(DbContextOptions<DBContextSample> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<SampleUser>
+    {
+        public void Configure(EntityTypeBuilder<SampleUser> builder)
+        {
+            builder.Property(x => x.FirstName).HasMaxLength(150);
+            builder.Property(x => x.LastName).HasMaxLength(150);
+            builder.Property(x => x.Company).HasMaxLength(150);
+            builder.Property(x => x.Phone).HasMaxLength(150);
+
+
+        }
+    }
+}
---
+++ Areas/Identity/Data/SampleUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ManagementSystem.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the SampleUser class
+public class SampleUser : IdentityUser
+{
+    public string FirstName
+    {
+        get; set;
+    }
+
+    public string LastName 
+    {
+        get; set;
+     }
+
+    public string Company
+    {
+        get; set;
+    }
+
+    public string Phone
+    {
+        get; set;   
+    }
+
+    }
+
